03909542d0f58f29419a0c40d5d34f5a77189f95
Yang Yu <yangyu@berkeley.edu>
2016-03-22 04:11:26 +0000

    validation

--- app.py
+++ app.py
@@ -21,17 +21,19 @@ class summary:
 		return render.index(body="",title="",date="",author="")
 	def POST(self):
 		form = web.input()
+		if(form.url == None or form.lines == None or form.url == "" or form.lines == ""):
+			return render.index(body="",title="",date="",author="")
 		url = 'https://api.aylien.com/api/v1/summarize?url=%s&sentences_number=%s' %(form.url,form.lines)
 		payload = {}
-		head = { "X-AYLIEN-TextAPI-Application-Key": "INSERT KEY HERE","X-AYLIEN-TextAPI-Application-ID": "INSERT ID HERE"}
+		head = { "X-AYLIEN-TextAPI-Application-Key": "68188d3a9d107b0f84480d7429a463f5","X-AYLIEN-TextAPI-Application-ID": "d974775d"}
 		r = requests.post(url, data=json.dumps(payload), headers=head)
 		smry = r.json()["sentences"]
 		val = ""
 		for s in smry:
-			val+=(s+" ");
+			val+=(s+"\n");
 		url = 'https://api.aylien.com/api/v1/extract?url=%s' %(form.url)
 		payload = {}
-		head = { "X-AYLIEN-TextAPI-Application-Key": "INSERT KEY HERE","X-AYLIEN-TextAPI-Application-ID": "INSERT ID HERE"}
+		head = { "X-AYLIEN-TextAPI-Application-Key": "68188d3a9d107b0f84480d7429a463f5","X-AYLIEN-TextAPI-Application-ID": "d974775d"}
 		r = requests.post(url, data=json.dumps(payload), headers=head)
 		title = r.json()["title"]
 		author = r.json()["author"]
